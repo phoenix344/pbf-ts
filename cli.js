57d796f720685d21ba6d45a42c6f9a0e6ecef31a
pheonix344 <pheonix34@protonmail.com>
2018-09-12 00:40:23 +0200

    add CLI test

--- cli.js
+++ cli.js
@@ -1,18 +1,26 @@
 #!/usr/bin/env node
-const { join } = require('path')
-const { writeSchemaFile } = require('./index');
-const { readAsync } = require('./util');
-let [source, target, ...files] = process.argv.slice(2);
-
-if (!source || !target || !files || !files.length) {
-    console.log('pbfts <source dir> <target dir> <pbf file 1> <pbf file 2> ...');
-    console.log('example: pbfts ./src ./lib file1.proto file2.proto');
-    return;
-}
 
 (async () => {
-    const content = await files.map(file => readAsync(join(source, file)));
-    await content.map(cnt => writeSchemaFile(cnt));
-});
+    const { join } = require('path')
+    const { writeSchemaFile } = require('./index');
+    const { readAsync } = require('./util');
+    let [source, target, ...files] = process.argv.slice(2);
 
-console.log('done!');
+    if (!source || !target || !files || !files.length) {
+        console.log('pbfts <source dir> <target dir> <pbf file 1> <pbf file 2> ...');
+        console.log('example: pbfts ./src ./lib file1.proto file2.proto');
+        return;
+    }
+
+    try {
+        const content = await Promise.all(files.map(file => readAsync(join(source, file))));
+        console.log(content[0].toString());
+        await Promise.all(content.map(cnt => writeSchemaFile(cnt, { writeDir: target })));
+        console.log('done!');
+        process.exit();
+    } catch (err) {
+        console.error(`(ノಠ益ಠ)ノ彡┻━┻`);
+        console.error(err.stack);
+        process.exit(1);
+    }
+})().catch(console.error);
--- test/cli.spec.js
+++ test/cli.spec.js
@@ -1,1 +1,11 @@
-module.exports = [];
+const { unlinkSync, existsSync } = require('fs');
+const { execSync } = require('child_process');
+const assert = require('assert');
+
+module.exports = [
+    (async function testCLI() {
+        execSync('./cli.js ./test/source ./test/target 01-cli.proto');
+        assert.ok(existsSync('./test/target/X.ts'));
+        unlinkSync('./test/target/X.ts');
+    })()
+];
