00a4416e938084c42e9b692425a272c8590f6def
pheonix344 <pheonix34@protonmail.com>
2018-09-12 00:19:30 +0200

    test ts-file creation synchronously

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,5 @@
 const { readFileSync, unlinkSync } = require('fs');
-const { createDefinition, writeSchemaFile } = require('../index');
+const { createDefinition, writeSchemaFile, writeSchemaFileSync } = require('../index');
 const assert = require('assert');
 
 module.exports = [
@@ -103,5 +103,101 @@ export const C = {
         unlinkSync('test/B.ts');
         unlinkSync('test/C.ts');
 
+    })(),
+    (async function testWriteSchemaFileSync() {
+        writeSchemaFileSync(`
+message A {
+    required int32 a = 1;
+    optional int32 b = 2;
+}
+message B {
+    required string username = 1;
+    required bool active = 2;
+}
+message C {
+    required double x = 1;
+    optional double y = 2;
+}
+`, {writeDir: 'test'});
+
+        assert.equal(readFileSync('test/A.ts').toString(), `import Pbf from "pbf";
+
+export interface ASchema {
+    a: number;
+    b?: number;
+}
+
+export const A = {
+    decode(buf: Buffer | Uint8Array): ASchema {
+        const pbf = new Pbf(buf);
+        return pbf.readFields<ASchema>((tag: number, obj?: ASchema, pbf?: Pbf) => {
+            if (tag === 1 && obj && pbf) { obj.a = pbf.readVarint(); }
+            else if (tag === 2 && obj && pbf) { obj.b = pbf.readVarint(); }
+        }, { a: 0, b: 0 });
+    },
+
+    encode(obj: ASchema): Uint8Array {
+        const pbf = new Pbf();
+        if (obj.a) { pbf.writeVarintField(1, obj.a); }
+        if (obj.b) { pbf.writeVarintField(2, obj.b); }
+        const buffer = pbf.finish();
+        return buffer;
+    }
+};`);
+
+        assert.equal(readFileSync('test/B.ts').toString(), `import Pbf from "pbf";
+
+export interface BSchema {
+    username: string;
+    active: boolean;
+}
+
+export const B = {
+    decode(buf: Buffer | Uint8Array): BSchema {
+        const pbf = new Pbf(buf);
+        return pbf.readFields<BSchema>((tag: number, obj?: BSchema, pbf?: Pbf) => {
+            if (tag === 1 && obj && pbf) { obj.username = pbf.readString(); }
+            else if (tag === 2 && obj && pbf) { obj.active = pbf.readBoolean(); }
+        }, { username: "", active: false });
+    },
+
+    encode(obj: BSchema): Uint8Array {
+        const pbf = new Pbf();
+        if (obj.username) { pbf.writeStringField(1, obj.username); }
+        if (obj.active) { pbf.writeBooleanField(2, obj.active); }
+        const buffer = pbf.finish();
+        return buffer;
+    }
+};`);
+
+        assert.equal(readFileSync('test/C.ts').toString(), `import Pbf from "pbf";
+
+export interface CSchema {
+    x: number;
+    y?: number;
+}
+
+export const C = {
+    decode(buf: Buffer | Uint8Array): CSchema {
+        const pbf = new Pbf(buf);
+        return pbf.readFields<CSchema>((tag: number, obj?: CSchema, pbf?: Pbf) => {
+            if (tag === 1 && obj && pbf) { obj.x = pbf.readDouble(); }
+            else if (tag === 2 && obj && pbf) { obj.y = pbf.readDouble(); }
+        }, { x: 0, y: 0 });
+    },
+
+    encode(obj: CSchema): Uint8Array {
+        const pbf = new Pbf();
+        if (obj.x) { pbf.writeDoubleField(1, obj.x); }
+        if (obj.y) { pbf.writeDoubleField(2, obj.y); }
+        const buffer = pbf.finish();
+        return buffer;
+    }
+};`);
+
+        unlinkSync('test/A.ts');
+        unlinkSync('test/B.ts');
+        unlinkSync('test/C.ts');
+
     })()
-]
+];
